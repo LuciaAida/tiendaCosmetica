f9ef426e3fd4d109c913f2762e37a9e204c1ae49
LuciaAida <luciapa799@gmail.com>
2025-02-27 17:27:03 +0100

    Info personal y cesta empezados (frontend)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,16 @@ import { ListaProductosComponent } from './components/main/producto/lista-produc
 import { LoginComponent } from './components/main/usuario/login/login.component';
 import { CrearCuentaComponent } from './components/main/usuario/crearCuenta/crear-cuenta.component';
 import { FormContactoComponent } from './components/footer/form-contacto/form-contacto.component';
+import { CestaCompraComponent } from './components/main/usuario/cesta-compra/cesta-compra.component';
+import { InfoPersonalComponent } from './components/main/usuario/info-personal/info-personal.component';
 
 export const routes: Routes = [
     { path: 'lista', component: ListaProductosComponent }, 
     { path: 'login', component: LoginComponent }, 
     { path: 'crearCuenta', component: CrearCuentaComponent }, 
     { path: 'formCont', component: FormContactoComponent }, 
+    { path: 'cestaComp', component: CestaCompraComponent },
+    { path: 'infoPers', component: InfoPersonalComponent },
     { path: '', redirectTo: '/lista', pathMatch: 'full' },   //redirige si no pones nada 
     { path: '**', redirectTo: '/lista'}  //redirige si pones algo inventado
 ];
